91ce2c5760ac112323699baab24ef8313853f3d9
mono motapa <bahobab@hotmail.com>
2018-04-14 13:09:03 -0500

    installed middleware

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const cors = require('cors');
+const bodyParser = require('bodyParser');
+const morgan = require('morgan');
+const errorHandler = require('errorhandler');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+app.use(errorHandler());
+
+
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite', (err) => {
     if (err) {
         console.log('Error creating/connecting to database', err);
