a63cb1b9cd66381ee28b65b8f22ab18987a11404
mono motapa <bahobab@hotmail.com>
2018-04-14 13:29:14 -0500

    added database connect for employee route

--- api/employee.js
+++ api/employee.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const employeeRouter = express.Router();
 
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite', (err) => {
+    if (err) {
+        console.log('Error connecting to database...');
+    } else {
+        console.log('Successfully connected to in-memory database');
+    }
+});
+
+
 
 module.exports = employeeRouter;
