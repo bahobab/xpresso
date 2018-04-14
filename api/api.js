32921816fe347793cbb5b2de30046136b30114c7
mono motapa <bahobab@hotmail.com>
2018-04-14 13:23:22 -0500

    added employee router

--- api/api.js
+++ api/api.js
@@ -1,4 +1,8 @@
 const express = require('express');
-const apiRouter = express.Router()
+const apiRouter = express.Router();
+
+const employeeRouter = require('./employee');
+
+apiRouter.use('/employee', employeeRouter);
 
 module.exports = apiRouter;
