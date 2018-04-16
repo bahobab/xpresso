ab8558b3af21b546577f0771e57ea25ffacdadfa
mono motapa <bahobab@hotmail.com>
2018-04-16 13:37:16 -0500

    POST: /api/employees/:employeeId/timesheets - employee tests commented out

--- api/timesheet.js
+++ api/timesheet.js
@@ -45,4 +45,61 @@ timesheetRouter.get('/', async (req, res, next) => {
     }
 });
 
+timesheetRouter.post('/', async (req, res, next) => {
+    const newTimesheet = req.body.timesheet;
+    const {hours, rate, date} = newTimesheet;
+    const employeeId = req.params.employeeId;
+    
+    if (!dbUtils.isValid(hours, rate, date)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const postQuery = `INSERT INTO
+                        Timesheet
+                            (
+                                hours,
+                                rate,
+                                date,
+                                employee_id
+                            )
+                    VALUES
+                        (
+                            $hours,
+                            $rate,
+                            $date,
+                            $employee_id
+                        );`;
+    const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employee_id: employeeId
+    };
+    
+    const selectOneEmployee = await dbUtils.selectOne(db, 'Employee', 'id', employeeId);
+    if (selectOneEmployee.err) {
+        next(selectOneEmployee.err);
+    } else {
+        if (!selectOneEmployee.data) {
+            res.sendStatus(404);
+        } else {
+            const postResults = await dbUtils.insertOne(db, postQuery, values);
+            if (postResults.err) {
+                next(err);
+            } else {
+                const createdTimesheet = await dbUtils.selectOne(db, 'Timesheet', 'id', postResults.lastID);
+                if (createdTimesheet.err) {
+                    next (createdTimesheet.err);
+                } else {
+                    res.status(201).json({timesheet:createdTimesheet.data});
+                }
+            }
+        }
+    }
+});
+
+dbUtils.routerParam(db, timesheetRouter, 'Timesheet', 'timesheet');
+
+
 module.exports = timesheetRouter;
